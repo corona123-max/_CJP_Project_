dc9ffea495ee1e91e0d6da56d2e6dc93751ce369
Vedang <vedanghemu@gmail.com>
2021-04-26 15:04:31 +0530

    commit

---
+++ Pizza_Java_Project/src/Pizza.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author win10
+ */
+public interface Pizza {
+    float checkCrust();
+    float hasVegetable();
+    //float hasCheese();
+    float hasTopping();
+    String cookongTime();
+    //float calDiscount();
+    float totalCost();
+}
